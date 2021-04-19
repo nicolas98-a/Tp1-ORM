99c626ad6a7628a8befa27de924122aeea758dfa
Nicolas <acu.nicolas.1998@gmail.com>
2021-04-19 13:34:30 -0300

    Se agrego migration y se creo la base de datos

--- Tp.Restaurante/Tp.Restaurante/Program.cs
+++ Tp.Restaurante/Tp.Restaurante/Program.cs
@@ -7,6 +7,7 @@ namespace Tp.Restaurante
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
